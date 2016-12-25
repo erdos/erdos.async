6279b91af5906fe1a66db05c037077b7cab2dd74
Janos Erdos <erdosj@gmail.com>
2016-12-25 12:10:11 +0100

    further ideas

--- src/erdos/async.clj
+++ src/erdos/async.clj
@@ -2,7 +2,6 @@
   (:require [clojure.core.async :refer [go chan <! >!] :as async]
             [clojure.core.async.impl.protocols :as impl]))
 
-;(def)
 
 (defn fn-chan [body-fn]
   (let [end? (atom false)]
@@ -19,17 +18,20 @@
                  result))
            (impl/take! fn1)))))))
 
+
 (defmacro go-fn
   "Createas a go block that evaluates its body on reads.
    Once it return nil it remains closed and body is never invoked again.
    Not thread-safe."
   [& bodies] `(fn-chan (fn [] ~@bodies)))
 
+
 (defmacro go-promise
   "A wrapper macro around promise-chan."
   [& bodies]
   `(doto (async/promise-chan)
      (-> (<! (do ~@bodies)) (go))))
+
 
 (defmacro go-delay
   "Creates a go block that evaluates its body on first take.
@@ -38,19 +40,54 @@
   `(let [c# (delay (go ~@bodies))]
      (go-fn (<! @c#))))
 
+(defn repeatedly-chan
+  "Like repeatedly but returns a channel that is closed on the first nil value."
+  [fn1]
+  (let [c (chan)]
+    (go (loop []
+          (when-some [x (fn1)]
+            (do (>! c x)
+                (recur))
+            (async/close! c))))
+    c))
+
 ;; TODO: impl this
 (defn directory-watch-chan
   "Returns a channel of file system change events for a given directory."
-  [directory]
-  (let []
-    (reify
-      impl/Channel
-      (closed? [_] false)
-      (close! [_] nil)
-      impl/ReadPort
-      (take! [_ fn1]
-        nil
-        ))))
+  ([directory] (directory-watch-chan directory 1))
+  ([directory buffer-size]
+   (let [
+         fs (java.io.file.FileSystems/getDefault)
+         ws (.newWatchService fs)
+         watchable ^java.nio.file.Watchable (dir->watchange directory)
+         events (chan buffer-size)
+         evts [java.nio.file.StandardWatchEventKinds/ENTRY_CREATE
+               java.nio.file.StandardWatchEventKinds/ENTRY_DELETE
+               java.nio.file.StandardWatchEventKinds/ENTRY_MODIFY]
+         watch-key (.register watchable (to-array evts))
+         parse-evt (fn [^java.io.file.WatchEvent evt]
+                     (bean evt))]
+     ;; elindithatunk egy szalat csak erre a celra.
+     ;; close eseten elzarjuk
+
+     ;; on stop: call watch-key.cancel
+
+     ;; thread events to channel
+     (async/thread
+       (doseq [events (repeatedly #(.pollEvents watch-key))
+               evt    events
+               :while (.isValid watch-key)]
+         (async/>!! events (parse-evt x)))
+       (async/close! events))
+
+     (reify
+       impl/Channel
+       (closed? [_] (.isValid watch-key))
+       (close! [_]  (.cancel watch-key))
+       impl/ReadPort
+       (take! [_ fn1]
+         (impl/take! events fn1))))))
+
 
 ;; example usage:
 ;; (partition-by-delay :file-name 5000 file-change-channel)
@@ -72,6 +109,7 @@
         (async/close! output)))
     output))
 
+
 (defn partition-delay [msecs input-chan]
   (let [output (async/chan)]
     (async/go-loop []
@@ -88,6 +126,13 @@
           (recur))
         (async/close! output)))
     output))
+
+
+(defn directory-watch-delayed-chan
+  "Channel of file names that are changed in a directory (delayed by 5000 ms)"
+  [directory]
+  (->> (file-watcher-channel directory)
+       (partition-by-delay (comp :file-name first) 5000)))
 
 
 (defmacro dochan
